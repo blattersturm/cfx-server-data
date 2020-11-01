b8c718f226330e2854b91f7d88e5e3277fe77440
blattersturm <peachypies@protonmail.ch>
2020-11-01 12:50:24 +0100

    gameplay: example money, money fountain, ped money drop and player ID systems

---
+++ resources/[gameplay]/ped-money-drops/fxmanifest.lua
@@ -0,0 +1,14 @@
+version '1.0.0'
+description 'An example money system client.'
+author 'Cfx.re <pr@fivem.net>'
+
+fx_version 'bodacious'
+game 'gta5'
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+lua54 'yes'
+
+-- temp
+map 'map.lua'
